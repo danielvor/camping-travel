4681840c2077cef641c7a95af4114c5c4f1c6ea6
Daniel Victor de Oliveira Rodrigues <oliveira0196@gmail.com>
2025-10-31 11:36:17 -0300

    main

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Início' },
+  { href: '/', key: 'how_hilink_work', label: 'Como o Hilink Funciona?' },
+  { href: '/', key: 'services', label: 'Serviços' },
+  { href: '/', key: 'pricing', label: 'Preços' },
+  { href: '/', key: 'contact_us', label: 'Contate-nos' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Mapas reais offline',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'Nós fornecemos uma solução para você poder usar nosso aplicativo durante escaladas. Sim, mapas offline que você pode usar a qualquer momento, mesmo sem sinal no local',
+  },
+  {
+    title: 'Defina um cronograma de aventura',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      'Agende uma aventura com amigos. Nos feriados, há muitas ofertas interessantes do Hilink. Dessa forma, não há mais discussões',
+  },
+  {
+    title: 'Tecnologia com realidade aumentada',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'A tecnologia usa realidade aumentada como guia para sua trilha de caminhada na floresta até o topo da montanha. Já compatível com a mais recente tecnologia sem necessidade de internet',
+  },
+  {
+    title: 'Muitos locais novos todo mês',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Muitos locais novos todo mês, porque temos uma comunidade mundial de alpinistas que compartilham suas melhores experiências com escalada',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Saiba Mais',
+    links: [
+      'Sobre o Hilink',
+      'Comunicados à Imprensa',
+      'Meio Ambiente',
+      'Trabalhe Conosco',
+      'Política de Privacidade',
+      'Contate-nos',
+    ],
+  },
+  {
+    title: 'Nossa Comunidade',
+    links: ['Escalada xixixi', 'Caminhada hilink', 'Hilink kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contate-nos',
+  links: [
+    { label: 'Administrador', value: '123-456-7890' },
+    { label: 'E-mail Oficial', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Redes Sociais',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
